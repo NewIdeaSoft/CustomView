1d067ee4bc1ca5a9302239dc6ed57eebaced9dc5
Yangtao <yangtao9651@163.com>
2017-11-02 07:33:20 +0800

    开关按钮

---
+++ androidlib/src/main/java/com/nisoft/androidlib/view/SwitchButton.java
@@ -0,0 +1,19 @@
+package com.nisoft.androidlib.view;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by NewIdeaSoft on 2017/11/1.
+ */
+
+public class SwitchButton extends View {
+    public SwitchButton(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
